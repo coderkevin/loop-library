0fa906827a2610e053c6eadfc5c3d518ae28768b
Kevin Killingsworth <kevin@redfenix.com>
2025-12-13 22:35:48 -0600

    Initial application

---
+++ src/renderer/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const App: React.FC = () => {
+  return (
+    <div style={{ padding: 24, fontFamily: "system-ui" }}>
+      <h1>Loop Library</h1>
+      <p>Empty Electron + React shell, ready to grow.</p>
+    </div>
+  );
+};
+
